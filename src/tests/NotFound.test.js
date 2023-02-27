2af0552336943594fb1cb919d8be25f4f0d3d41c
Sergio <sergioajk@hotmail.com>
2023-02-27 19:40:06 -0300

    req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('Testa o componente NotFound', () => {
+  test('Testa se a página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundMessage = screen.getByRole(
+      'heading',
+      { name: /Page requested not found/i },
+      { level: 2 },
+    );
+    expect(notFoundMessage).toBeInTheDocument();
+  });
+
+  test('Testa se a página mostra a imagem', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundImage = screen.getByRole('img');
+    expect(notFoundImage).toBeInTheDocument();
+    expect(notFoundImage).toHaveAttribute(
+      'src',
+      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+    );
+  });
+});
+
+// test('', () => {});
